4d4a8b8a62116be035645b389ff68ff4022ef019
n242320 <n242320@INFS117.inf.numazu-vtc.ac.jp>
2024-09-04 11:16:56 +0900

    update

--- JavaFromScotAc/src/Main.java
+++ JavaFromScotAc/src/Main.java
@@ -1,8 +1,15 @@
 
 public class Main {
+	
+	static void printArgs(String[] args) {
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		printArgs(args);
 	}
 
 }
